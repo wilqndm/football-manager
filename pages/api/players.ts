4563748420f761e605253f87c1d88fd82dc8197a
wilqndm <wilq.ndm@gmail.com>
2025-07-23 23:39:29 +0200

    Update players.ts

--- pages/api/players.ts
+++ pages/api/players.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
-
 const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
